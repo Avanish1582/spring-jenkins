b621d93ecd2f9165dba4e435d175544ed7f5556d
Avanish <87369424+Avanishmnit@users.noreply.github.com>
2024-03-06 22:29:53 +0530

    commit first

--- src/test/java/com/example/SpringbootDockerproject/SpringbootDockerprojectApplicationTests.java
+++ src/test/java/com/example/SpringbootDockerproject/SpringbootDockerprojectApplicationTests.java
@@ -11,6 +11,10 @@ class SpringbootDockerprojectApplicationTests {
 	public static Logger logger= LoggerFactory.getLogger(SpringbootDockerprojectApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test case Executin 1");
+		logger.info("Test case Executin 2");
+		logger.info("Test case Executin 2");
+
 	}
 
 }
